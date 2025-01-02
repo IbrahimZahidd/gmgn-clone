2f97ae178089bd01a5a9ac19f35f5a5bbe31d2c8
IbrahimZahidd <buttibrahim848@.com>
2025-01-03 02:42:41 +0500

    Initial commit

--- gmgn-frontend/gmgn-frontend/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{vue,js,jsx,ts,tsx}", "./public/index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
